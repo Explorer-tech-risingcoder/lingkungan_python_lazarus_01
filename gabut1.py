af121bb0cb317ecd8ebe04f71e9a243b3526d97f
lazarus <kojekpermen0@gmail.com>
2026-08-25 13:31:59 +0700

    menjelang siang di selasa ini cihuy dahlah, besok selasa

--- gabut1.py
+++ gabut1.py
@@ -1,22 +1,43 @@
 
+
 def user():
-    print("ketik nama anda")
-    pengguna = input("> ")
     
 
-#gw mau buat kalo pengguna ngetik simbol aneh aneh nanti ada sesuatu kondisi
-
-    if pengguna.strip() == "": # strip itu cuma buat menghapus spasi kosong e
-        print("\n")
-        print("[!] input kosong, silahkan coba lagi")
-    
-    elif pengguna.isalpha() : #
-        print(pengguna)
-        print(f"{pengguna.isalpha()}")
-    else:
-        print(f"{pengguna.isalpha()}")
+    #gw mau buat kalo pengguna ngetik simbol aneh aneh nanti ada sesuatu kondisi
+    while True:
+        print("ketik nama anda")
+        pengguna = input("> ") 
+        if pengguna.strip() == "" : # strip itu cuma buat menghapus spasi kosong 
+            print("\n")
+            print("[!] input kosong, silahkan coba lagi")
+        
+        elif pengguna.isalpha() : #
+            print(f"halo, {pengguna}")
+            print(f"kondisi {pengguna.isalpha()}")
+        else:
+            print(f"{pengguna.isalpha()}")
+            #break
 user()
 
 #testing kode
 #pengguna = "furqon".isalpha()
 #print(f"{pengguna}")
+
+# penggunaan cara kerja strip() bukan stript() yaa,
+
+# >>> "muhammad furqon".strip()
+# 'muhammad furqon'
+# >>> " muham mmad fu rqon".strip()
+# 'muham mmad fu rqon'
+# >>> "     muhammad furqon  ".strip()
+# 'muhammad furqon'
+# >>> 
+#
+
+
+#simbol ini == artinya perbandingan misal 'p' == 'p' output True kalo p nya sebelah berbeda maka False 
+# >>> "p" == "p"
+# True
+# >>> "p" == "P"
+# False
+# >>> 

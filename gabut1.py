7d8f003a228536c16963983d87af20f3de81ed59
lazarus <kojekpermen0@gmail.com>
2026-08-25 08:35:46 +0700

    manfaatin libur kejepit dlu ga sih kekkeke

--- gabut1.py
+++ gabut1.py
@@ -6,9 +6,17 @@ def user():
 
 #gw mau buat kalo pengguna ngetik simbol aneh aneh nanti ada sesuatu kondisi
 
-    if pengguna == "":
+    if pengguna.strip() == "": # strip itu cuma buat menghapus spasi kosong e
         print("\n")
         print("[!] input kosong, silahkan coba lagi")
+    
+    elif pengguna.isalpha() : #
+        print(pengguna)
+        print(f"{pengguna.isalpha()}")
     else:
-        print(f"halo, {pengguna}")
+        print(f"{pengguna.isalpha()}")
 user()
+
+#testing kode
+#pengguna = "furqon".isalpha()
+#print(f"{pengguna}")

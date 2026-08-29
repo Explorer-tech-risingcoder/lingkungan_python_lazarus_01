62d7a964ab3c4b09296ed68c4e2115332d433b73
lazarus <kojekpermen0@gmail.com>
2026-08-30 01:55:47 +0700

    malam minggu ngoding uwaw perkuat fundamental aja hehe

---
+++ daftar-login.py
@@ -0,0 +1,33 @@
+#pada program ini gw akan membuat daftar dan login akun tanpa database dulu
+#kedepannya gw baru akan hubungin ke database
+# step by step letsgo!
+
+
+def daftar_akun():
+    print("\n")
+    print("daftar akun")
+
+    print("\n")
+
+    print("buat nama anda")
+    pengguna_baru = input("> ")
+
+    print("\n")
+
+    print("buat kode rahasia milik anda (maksimal 12 karakter)")
+    kode_baru = input
+#daftar_akun()
+
+
+def uji_coba_kode():
+    print("hello world")
+
+
+    angka = 0
+    for i in range(5):
+        angka += 2 
+        print(f"putaran: {angka}")
+    print("\n") 
+    print(angka)
+uji_coba_kode()
+
